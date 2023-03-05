d977231f8e3c89ba4f69cd35356f937526f1529e
Merve-kz <merve.krgoz@hotmail.com>
2023-03-05 18:40:42 +0100

    add a interface and define the title, image and content inside the component

--- src/app/itemcategory/itemcategory.component.ts
+++ src/app/itemcategory/itemcategory.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+// Define an interface for a card object
+interface Card {
+  title: string;
+  imageUrl: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-itemcategory',
   templateUrl: './itemcategory.component.html',
   styleUrls: ['./itemcategory.component.scss']
 })
+
+
 export class ItemcategoryComponent {
 
+
 }
+
+
+
